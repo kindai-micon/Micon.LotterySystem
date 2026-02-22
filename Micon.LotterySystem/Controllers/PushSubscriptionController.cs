553a6c66affc723af727b2b5e4f6dc8a327eaa20
bucheapp <bucheapp@gmail.com>
2026-02-22 13:04:00 +0900

    骨組みの作成

---
+++ Micon.LotterySystem/Controllers/PushSubscriptionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Micon.LotterySystem.Controllers
+[ApiController]
+[Route("api/push-subscription")]
+{
+	public PushSubscriptionController(ApplicationDbContext applicationDbContext) : ControllerBase
+	{
+		[HttpPost("{guid}")]
+		public async Task<IActionResult> Subscribe([FromRoute] Guid guid,[FromBody] PushSubscriptionDTO subscription,)
+		{
+        //TODO : 保存された購読情報をデータベースに保存するロジックを実装する
+    }
+}
+}
