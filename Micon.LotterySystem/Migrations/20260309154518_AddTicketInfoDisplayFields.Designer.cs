c6348b62fc175680611836af9cb2d1ee2c9c5767
actbit <57023457+actbit@users.noreply.github.com>
2026-03-10 10:32:19 +0900

    [fix] migrations

--- Micon.LotterySystem/Migrations/20260309154518_AddTicketInfoDisplayFields.Designer.cs
+++ Micon.LotterySystem/Migrations/20260309154518_AddTicketInfoDisplayFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Micon.LotterySystem;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Micon.LotterySystem.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260309154518_AddTicketInfoDisplayFields")]
+    partial class AddTicketInfoDisplayFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
